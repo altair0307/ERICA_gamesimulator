a866ac22dd5aac66ec7172d9c67ac80e7954794e
seohong choi <ds07017@naver.com>
2019-11-27 20:16:39 +0900

    class Player add

--- main.java
+++ main.java
@@ -1,3 +1,16 @@
+class Player{
+	protected double Roamingstat;
+	protected double Csstat;
+	protected double Skillstat;
+	protected double Objectstat;
+
+	public Player(double r, double c, double s, double o) {
+		Roamingstat = r;
+		Csstat = c;
+		Skillstat = s;
+		Objectstat = o;
+	}
+}
 class Main {
     public static void main(String[] args) {
             System.out.println("hello, world!");
